a9271d60bd8c8f5e15b211d9067142c7832b3d83
Atanes-Trybe <alexandre.atanes@betrybe.com>
2022-08-06 18:42:52 -0300

    Inclusão da pasta para organização dos componentes

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import Header from '../Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title='Controle de estoque - App'/>
+    </div>
+  );
+}
+
+export default App;
