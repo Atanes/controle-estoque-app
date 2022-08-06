bb486997256de79a85d6d77eeae63b6c6bbd051f
Atanes-Trybe <alexandre.atanes@betrybe.com>
2022-08-06 18:43:09 -0300

    Inclusão do componente Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Header.css';
+
+const Header = (props) => {
+    return <header className="estoque-header">
+        <h1>{ props.title }</h1>
+    </header>
+}
+
+export default Header;
