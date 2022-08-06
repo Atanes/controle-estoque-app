8d1f0358c1c6194faff171670cdc0a72dc627955
Atanes-Trybe <alexandre.atanes@betrybe.com>
2022-08-06 19:36:35 -0300

    Inclusão do componente Section na chamada do APP

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import Header from '../Header/Header';
+import Section from '../Section/Section';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header title='Controle de estoque - App'/>
+      <Header title='Controle de estoque - App' />
+      <Section />
     </div>
   );
 }
